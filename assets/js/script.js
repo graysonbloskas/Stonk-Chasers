16fd580596e8bfe7870af771fa790771e540f4dd
CGalko <cash.galko12@ncf.edu>
2021-04-07 10:20:36 -0400

    Honestly Im not sure what I changed here but everything is working properly, so gonna commit it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-// var apiTest = function () {
-//     var apiUrl = 'https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol=BTC&apikey=' + alphaApiKey;
-
-//  fetch(apiUrl)
-//   .then(function (response) {
-//     return response.json();
-
-//   })
-//   .then(function (data) {
-//     console.log(data);
-    
-    
-//   });
-// }
 
 // Variable needed for autofill function. 
 var availableTags = []
