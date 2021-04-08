c7bdec7a6de141b3a70690203ce3561b19fda975
CGalko <cash.galko12@ncf.edu>
2021-04-07 20:06:47 -0400

    Added a function to check the currency value for the widget. Need to figure out how to get the widget to reload. Going to pause on this and ask tutor tomorrow. Need to progress some other stuff.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,66 +4,49 @@ $("#time-date").text(today.format("MMM Do, YYYY"));
 var time = moment().format("hh:mm A");
 $("#time-display").text(time);
 
+// Variable needed for autofill function. 
+// var availableTags = []
+// var searchBar = $('#search')
 
-// var apiTest = function () {
-//     var apiUrl = 'https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol=BTC&apikey=' + alphaApiKey;
+// // Auto complete fetch request provided through API. This function uses the input value from the search bar to use an autofill option.
 
+// var autofillTargets = function(searchValue) {
+//     // var apiUrl = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + searchValue + '&apikey='+ alphaApiKey
+//     var apiUrl = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tes&apikey='+ alphaApiKey
 
-//  fetch(apiUrl)
+//    fetch(apiUrl)
 //   .then(function (response) {
 //     return response.json();
 
 //   })
 //   .then(function (data) {
 //     console.log(data);
-    
+//     data.bestMatches.forEach(element => {
+//         var symbol = element['1. symbol']
+//         var name = element['2. name']
+//         console.log(symbol);
+//         console.log(name);
+//         // pushes the values into a global array used for autofill function
+//         availableTags.push(symbol);
+//         availableTags.push(name);
+//         console.log(availableTags);
+//     });
     
 //   });
 // }
 
-// Variable needed for autofill function. 
-var availableTags = []
-var searchBar = $('#search')
+// autofillTargets();
+// // startBtn.addEventListener(update, autofillTargets){
+// // capture user input
+// // autofillTargets(searchValue); 
+// // }
 
-// Auto complete fetch request provided through API. This function uses the input value from the search bar to use an autofill option.
+// $( function() {
 
-var autofillTargets = function(searchValue) {
-    // var apiUrl = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + searchValue + '&apikey='+ alphaApiKey
-    var apiUrl = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tes&apikey='+ alphaApiKey
-
-   fetch(apiUrl)
-  .then(function (response) {
-    return response.json();
-
-  })
-  .then(function (data) {
-    console.log(data);
-    data.bestMatches.forEach(element => {
-        var symbol = element['1. symbol']
-        var name = element['2. name']
-        console.log(symbol);
-        console.log(name);
-        // pushes the values into a global array used for autofill function
-        availableTags.push(symbol);
-        availableTags.push(name);
-        console.log(availableTags);
-    });
-    
-  });
-}
-
-autofillTargets();
-// startBtn.addEventListener(update, autofillTargets){
-// capture user input
-// autofillTargets(searchValue); 
-// }
-
-$( function() {
-
-    searchBar.autocomplete({
-      source: availableTags
-    });
-  } );
+//     searchBar.autocomplete({
+//       source: availableTags
+//     });
+//   } );
 
  
   
@@ -77,3 +60,44 @@ else if(id && id === 'advanced-search') {
 element && element.classList.remove('hidden');
 }
 }
+
+// Series of if / else statements that change the display currency of the ticker depending on user choice.
+// Cant figure out how to get the actual widget to refresh so it displays the new lcw-base value.
+var currencySelectorEl = document.querySelector('#form-stacked-select')
+function toggleCurrency(event) {  
+event.preventDefault();
+var tickerEl = document.querySelector('#ticker')
+console.log(currencySelectorEl.value);
+
+if (currencySelectorEl.value == 'USD') {
+  tickerEl.setAttribute('lcw-base', "USD");
+  
+}
+ else if ( currencySelectorEl.value == 'EUR') {
+  tickerEl.setAttribute('lcw-base', 'EUR');
+  
+  
+}
+ else if ( currencySelectorEl.value == 'GBP') {
+  tickerEl.setAttribute('lcw-base', 'GBP');
+  
+  
+}
+ else if ( currencySelectorEl.value == 'JPY') {
+  tickerEl.setAttribute('lcw-base', 'JPY');
+  
+  
+}
+ else if ( currencySelectorEl.value == 'BTC') {
+  tickerEl.setAttribute('lcw-base', 'BTC');
+  
+  
+}
+ else if ( currencySelectorEl.value == 'ETH') {
+  tickerEl.setAttribute('lcw-base', 'ETH');
+  
+  
+}
+};
+
+currencySelectorEl.addEventListener('change', toggleCurrency);
