fa3885dfd824093422310008c2a4fe77382943c6
Grayson Bloskas <graysonbloskas@gmail.com>
2021-04-08 23:03:36 -0400

    Added null checks, formatted the response for the volume call, some other things. Removed console logs, cleaned up a little. Will add comments to code tomorrow.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ var updateTime = function() {
 setInterval(updateTime, 1000);
 
 // // Variable needed for autofill function. 
-// var availableTags = ['1ST', '2GIVE', '808', 'ABT', 'ABY', 'AC', 'ACT', 'ADA', 'ADX', 'ADT', 'AE', 'AEON', 'AGI', 'AGRS', 'AI', 'AID', 'AION', 'AIR', 'AKY', 'ALIS', 'AMBER', 'AMP', 'AMPL', 'ANC', 'ANT', 'APPC', 'APX', 'ARDR', 'ARK', 'ARN', 'AST', 'ATB', 'ATM', 'ATS', 'AUR', 'AVT', 'B3', 'BAT', 'BAY', 'BBR', 'BCAP', 'BCC', 'BCD', 'BCH', 'BCN', 'BCPT', 'BCX', 'BCY', 'BDL', 'BEE', 'BELA', 'BET', 'BFT', 'BIS', 'BITB', 'BITBTC', 'BITCNY', 'BITEUR', 'BITGOLD', 'BITSILVER', 'BITUSD', 'BIX', 'BLITZ', 'BLK', 'BLN', 'BLOCK', 'BLZ', 'BMC', 'BNB', 'BNT', 'BNTY', 'BOST', 'BOT', 'BQ', 'BRD', 'BRK', 'BRX', 'BTA', 'BTC', 'BTCD', 'BTCP', 'BTG', 'BTM', 'BTS', 'BTSR', 'BTX', 'BURST', 'BUSD', 'BUZZ', 'BYC', 'BYTOM', 'C20', 'CANN', 'CAT', 'CCRB', 'CDT', 'CFI', 'CHAT', 'CHIPS', 'CLAM', 'CLOAK', 'CMP', 'CMT', 'CND', 'CNX', 'COFI', 'COSS', 'COVAL', 'CRBIT', 'CREA', 'CREDO', 'CRW', 'CSNO', 'CTR', 'CTXC', 'CURE', 'CVC', 'DAI', 'DAR', 'DASH', 'DATA', 'DAY', 'DBC', 'DBIX', 'DCN', 'DCR', 'DDF', 'DENT', 'DFS', 'DGB', 'DGC', 'DGD', 'DICE', 'DLT', 'DMD', 'DMT', 'DNT', 'DOGE', 'DOPE', 'DOT', 'DRGN', 'DTA', 'DYB', 'DYN', 'EAC', 'EBST', 'EBTC', 'ECC', 'ECN', 'EDG', 'EDO', 'EFL', 'EGC', 'EKT', 'ELA', 'EGC', 'EDO', 'EFL', 'EGC', 'EKT', 'ELA', 'ELEC', 'ELF', 'ELIX', 'EMB', 'EMC', 'EMC2', 'ENG', 'ENJ', 'ENRG', 'EOS', 'EOT', 'EQT', 'ERC', 'ETC', 'ETH', 'ETHD', 'ETHOS', 'ETN', 'ETP', 'ETT', 'EVE', 'EVX', 'EXCL', 'EXP', 'FTC', 'FLDC', 'FLO', 'FLT', 'FRST', 'FTC', 'FUEL', 'FUN', 'GAM', 'GAME', 'GAS', 'GBG', 'GBX', 'GBYTE', 'GCR', 'GEO', 'GLD', 'GNO', 'GNT', 'GOLOS', 'GRC', 'GRT', 'GRS', 'GRWI', 'GTC', 'GTO', 'GUP', 'GVT', 'GXS', 'HBN', 'HEAT', 'HMQ', 'HPB', 'HSR', 'HUSH', 'HVN', 'HXX', 'ICN', 'ICX', 'IFC', 'IFT', 'IGINS', 'INCNT', 'IND', 'INF', 'INK', 'INS', 'INSTAR', 'INT', 'INXT', 'IOC', 'ION', 'IOP', 'IOST', 'IOTA', 'IOTX', 'IQT', 'ITC', 'IXC', 'IXT', 'J8T', 'JNT', 'KCS', 'KICK', 'KIN', 'KMD', 'KNC', 'KORE', 'LBC', 'LCC', 'LEND', 'LEV', 'LGD', 'LINDA', 'LINK', 'LKK', 'LMC', 'LOCI', 'LOOM', 'LRC', 'LSK', 'LTC', 'LUN', 'MAID', 'MANA', 'MAX', 'MBRS', 'MCAP', 'MCO', 'MDA', 'MEC', 'MED', 'MEME', 'MER', 'MGC', 'MGO', 'MINEX', 'MINT', 'MITH', 'MKR', 'MLN', 'MNE', 'MNX', 'MOD', 'MONA', 'MRT', 'MSP', 'MTH', 'MTN', 'MUE', 'MUSIC', 'MYB', 'MYST', 'MZC', 'NAMO', 'NANO', 'NAS', 'NAV', 'NBT', 'NCASH', 'NDC', 'NEBL', 'NEO', 'NEOS', 'NET', 'NLC2', 'NLG', 'NMC', 'NMR', 'NOBL', 'NOTE', 'NPXS', 'NSR', 'NTO', 'NULS', 'NVC', 'NXC', 'NXT', 'OAX', 'OBITS', 'OCL', 'OCN', 'ODEM', 'ODN', 'OF', 'OK', 'OMG', 'OMNI', 'ONION', 'ONT', 'OPT', 'OST', 'PART', 'PASC', 'PAY', 'PBL', 'PBT', 'PFR', 'PING', 'PINK', 'PIVX', 'PIX', 'PLBT', 'PLR', 'PLU', 'POA', 'POE', 'POLY', 'POSW', 'POT', 'POWR', 'PPC', 'PPT', 'PPY', 'PRG', 'PRL', 'PRO', 'PST', 'PTC', 'PTOY', 'PURA', 'QASH', 'QAU', 'QLC', 'QRK', 'QRL', 'QSP', 'QTL', 'QTUM', 'QWARK', 'R', 'RAIN', 'RBIES', 'RBX', 'RBY', 'RCN', 'RDD', 'RDN', 'REC', 'RED', 'REP', 'REQ', 'RHOC', 'RIC', 'RISE', 'RLC', 'RLT', 'RPX', 'RRT', 'RUFF', 'RUP', 'RVT', 'SAFEX', 'SALT', 'SAN', 'SBD', 'SBTC', 'SC', 'SEELE', 'SEQ', 'SHIFT', 'SIB', 'SIGMA', 'SIGT', 'SJCX', 'SKIN', 'SKY', 'SLR', 'SLS', 'SMART', 'SMT', 'SNC', 'SOC', 'SOUL', 'SPANK', 'SPC', 'SPHR', 'SPR', 'SNX', 'SRN', 'START', 'STEEM', 'STK', 'STORJ', 'SOTRM', 'STQ', 'STRAT', 'STX', 'SUB', 'SWFTC', 'SWT', 'SYNX', 'SYS', 'TAAS', 'TAU', 'TCC', 'TFL', 'THC', 'THETA', 'TIME', 'TIX', 'TKN', 'TKR', 'TKS', 'TNB', 'TNT', 'TOA', 'TRAC', 'TRC', 'TRCT', 'TRIG', 'TRST', 'TRUE', 'TRUST', 'TRX', 'TUSD', 'TX', 'UBQ', 'UKG', 'ULA', 'UNB', 'UNITY', 'UNO', 'UNY', 'UP', 'URO', 'USDT', 'UTK', 'VEE', 'VEN', 'VERI', 'VIA', 'VIB', 'VIBE', 'VIVO', 'VOISE', 'VOX', 'VPN', 'VRC', 'VRM', 'VRS', 'VSL', 'VTC', 'VTR', 'WABI', 'WAN', 'WAVES', 'WAX', 'WCT', 'WDC', 'WGO', 'WGR', 'WINGS', 'WPR', 'WTC', 'WTT', 'XAS', 'XAUR', 'XBC', 'XBY', 'XCN', 'XCP', 'XDN', 'XEL', 'NEM', 'NHV', 'XID', 'XLM', 'XMG', 'XMR', 'XMT', 'XMY', 'XRL', 'XRP', 'XPEC', 'XST', 'XTZ', 'XUC', 'XVC', 'XVG', 'XWC', 'XZC', 'XZR', 'YEE', 'YOYOW', 'ZCC', 'ZCO', 'ZEC', 'ZEN', 'ZET', 'ZIL', 'ZLA', 'ZRX']
+
 var searchBar = $('#search');
 var enterBtn = $('#generate-card');
 var dropdownResponse = [];
@@ -60,14 +60,12 @@ function setDropdown() {
 
     searchBar.autocomplete({
       source: availableTags, 
-      response: function(event, ui) {
+      response: function(_, ui) {
       dropdownResponse = ui.content;
-
-
       }
 
     });
-    searchBar.on("autocompletesearch", function(event, ui){
+    searchBar.on("autocompletesearch", function(_, ui){
 
     })
   }
@@ -181,27 +179,28 @@ var generateCardHandler = async function (event) {
 
 async function awaitData(cl, ct){
   var data = await Promise.all([getHealthIndex(cl), getVolume(cl), getCurrentPrice(cl)]);
-  console.log(data);
   if(!!data){
     return setCryptoCard(ct, cl, data);
   }
   return data;
 }
 function setCryptoCard(cryptoType, cryptoLabel, res) {
-  console.log(res);
   var icon = 'https://www.iconpacks.net/icons/2/free-cryptocurrency-coin-icon-2422-thumb.png';
   var health = res && res[0];
+  var highLowData = res && res[1];
   var currentCurrency = currencySelectorEl.value;
   var currPrice = res && res[2];
+  var dayHighKey = '2a. high (' + currentCurrency + ')';
+  var dayLowKey = '3a. low (' + currentCurrency + ')';
   var card = {
     icon: icon, //going to replace with api
     health_index: health,
     name: cryptoType, //going to add the api
     ticker: cryptoLabel, //going to add api
     current_price: currPrice[currentCurrency], //api
-    day_high: 60000, //api
-    day_low: 50000, //api
-    day_vol: 6000000
+    day_high: highLowData[dayHighKey] || 'Unavailable', //api
+    day_low: highLowData[dayLowKey] || 'Unavailable', //api
+    day_vol: highLowData['5. volume']
   }; 
 
 
@@ -282,7 +281,6 @@ function populateCard(element) {
     cardBody.append(...pArr);
     cardMainDiv.append(cardBody);
 
-    console.log(cardMainDiv);
     var cardData = document.getElementById('main-body');
     return cardData.append(cardMainDiv);
 
@@ -305,7 +303,7 @@ async function getHealthIndex(cryptoType) {
  })
 .then(function (data) {
     var res = data;
-  var val =  res['Crypto Rating (FCAS)']['3. fcas rating'];
+  var val = res && res['Crypto Rating (FCAS)']['3. fcas rating'] || {};
   if(Object.keys(val).length === 0){
     return 'Not Available';
   }
@@ -327,12 +325,14 @@ async function getVolume(cryptoType) {
 })
 .then(function (data) {
   var res = data;
-var val =  res['Time Series (Digital Currency Daily)'];
+var val =  res['Time Series (Digital Currency Daily)'] || data;
 if(Object.keys(val).length === 0){
   return 'Not Available';
 }
-var _data = [val];
-return _data[_data.length - 1];
+var _data = val && Object.keys(val).map(x => {
+  return x && val[x];
+});
+return _data && _data[0];
 
 });
 }
